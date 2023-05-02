0bde55129e17a31e24272713872b8aa75f0ac684
sourav kumar pandey <soukumpan@gmail.com>
2023-05-02 07:53:07 +0530

    login ui done

--- lib/views/screens/auth/login_screen.dart
+++ lib/views/screens/auth/login_screen.dart
@@ -19,6 +19,7 @@ class LoginScreen extends StatelessWidget {
         body: Container(
       alignment: Alignment.center,
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text(
             'Tiktok Clone',
@@ -37,6 +38,8 @@ class LoginScreen extends StatelessWidget {
           ),
           const SizedBox(height: 25,),
           Container(
+            width: MediaQuery.of(context).size.width,
+            margin: const EdgeInsets.symmetric(horizontal: 20),
             child: TextInputField(
               controller: _emailController,
               isObscure: false,
@@ -44,6 +47,52 @@ class LoginScreen extends StatelessWidget {
               icon: Icons.email,
               ),
           ),
+          const SizedBox(height: 25,),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            margin: const EdgeInsets.symmetric(horizontal: 20),
+            child: TextInputField(
+              controller: _passwordController,
+              isObscure: true,
+              lableText: 'Password' ,
+              icon: Icons.password,
+              ),
+          ),
+           const SizedBox(height: 30,),
+          Container(
+            width: MediaQuery.of(context).size.width -40,
+            height: 50,
+            decoration: BoxDecoration(
+              color: buttonColor,
+              borderRadius: const BorderRadius.all(
+                Radius.circular(5),
+              ),
+            ),
+            
+            child: InkWell(
+              onTap: () {
+                
+              },
+              child: const Center(
+                child: Text('Login',style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w700,
+                ),),
+              ),
+            ),
+          ),
+          const SizedBox( height: 15,),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+
+          children: [
+            const Text('Don\'t have an account ?', style: TextStyle(fontSize: 20,),),
+            InkWell
+            (onTap: () {
+              
+            },
+              child: Text('Register', style: TextStyle(fontSize: 20,color: buttonColor),))
+        ],)
         ],
       ),
     ));
